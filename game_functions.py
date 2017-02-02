f78cfd5adba36867608aae8c5fbca300675670da
Gmes23 <gmesda23@gmail.com>
2017-02-02 00:14:24 -0500

    hero ship can now move left or right on bottom x-plane/ready to add firing bullet code

--- game_functions.py
+++ game_functions.py
@@ -8,24 +8,17 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship_moving = False
-            elif event.key == pygame.K_LEFT:
-                ship_moving = False
+            check_keyup_events(event, ship)
 
-def check_keydown_events(events, ship):
+def check_keydown_events(event, ship):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
-def check_keyup_events(events, ship):
+def check_keyup_events(event, ship):
     """Respond to key releases."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
